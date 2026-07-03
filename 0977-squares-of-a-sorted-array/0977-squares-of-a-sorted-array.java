156de8a2cb9d40a2726fc769e91a84a0347d31c2
Shivam Agrawal <160855618+Shivam-agarawal@users.noreply.github.com>
2026-07-03 13:24:48 +0530

    Time: 1 ms (100%), Space: 48.5 MB (13.88%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,4 +1,16 @@
 class Solution {
+    public static void reverse(int[] array) {
+    int start = 0;
+    int end = array.length - 1;
+    
+    while (start < end) {
+        int temp = array[start];
+        array[start] = array[end];
+        array[end] = temp;
+        start++;
+        end--;
+    }
+}
     public static int[] square(int[] nums) {
     int[] result = new int[nums.length];
     for (int i = 0; i < nums.length; i++) {
@@ -7,13 +19,22 @@ class Solution {
     return result;
 }
     public int[] sortedSquares(int[] nums) {
-        // int n = nums.length;
-        // int left= 0 , right = n-1;
+        int n = nums.length;
+        int left= 0 , right = n-1;
+        int k = 0;
         int[] result = square(nums);
-        // while(left<right){
-            
-        // }
-        Arrays.sort(result);
-        return result;
+        int[] ans = new int[n];
+        while(left<=right){
+          if(result[left] > result[right]){
+          ans[k++] = result[left];
+          left++;
+            }
+        else {
+            ans[k++] = result[right];
+            right--;
+            }
+        }
+        reverse(ans);
+        return ans;
     }
 }
