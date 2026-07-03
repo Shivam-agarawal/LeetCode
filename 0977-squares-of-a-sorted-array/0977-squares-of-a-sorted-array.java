3bded82e6482d67009f1ac1050abd825f26067b7
Shivam Agrawal <160855618+Shivam-agarawal@users.noreply.github.com>
2026-07-03 13:47:25 +0530

    Time: 1 ms (100%), Space: 47 MB (94.14%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,40 +1,39 @@
 class Solution {
-    public static void reverse(int[] array) {
-    int start = 0;
-    int end = array.length - 1;
+//     public static void reverse(int[] array) {
+//     int start = 0;
+//     int end = array.length - 1;
     
-    while (start < end) {
-        int temp = array[start];
-        array[start] = array[end];
-        array[end] = temp;
-        start++;
-        end--;
-    }
-}
-    public static int[] square(int[] nums) {
-    int[] result = new int[nums.length];
-    for (int i = 0; i < nums.length; i++) {
-        result[i] = nums[i] * nums[i];
-    }
-    return result;
-}
+//     while (start < end) {
+//         int temp = array[start];
+//         array[start] = array[end];
+//         array[end] = temp;
+//         start++;
+//         end--;
+//     }
+// }
+//     public static int[] square(int[] nums) {
+//     int[nums = new int[nums.length];
+//     for (int i = 0; i < nums.length; i++) {
+//       nums[i] = nums[i] * nums[i];
+//     }
+//     returnums;
+// }
     public int[] sortedSquares(int[] nums) {
         int n = nums.length;
         int left= 0 , right = n-1;
-        int k = 0;
-        int[] result = square(nums);
+        int k = n-1;
+        //int[nums = square(nums);
         int[] ans = new int[n];
         while(left<=right){
-          if(result[left] > result[right]){
-          ans[k++] = result[left];
+          if(nums[left]*nums[left] > nums[right]*nums[right]){
+          ans[k--] =  nums[left]*nums[left];
           left++;
             }
         else {
-            ans[k++] = result[right];
+            ans[k--] = nums[right]*nums[right];
             right--;
             }
         }
-        reverse(ans);
         return ans;
     }
 }
